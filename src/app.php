4890b93ec6a9152da7140877008aa2339f7b09d7
Brent Shaffer <betterbrent@google.com>
2018-02-07 22:29:56 -0800

    commit that actually gets it working

--- src/app.php
+++ src/app.php
@@ -1,48 +1,41 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider());
+$app['twig.path'] = [ __DIR__ . '/../views' ];
 
-$app->get('/', function () use ($app) {
-    $twigVars = [];
-    $form = Symfony\Component\Form\Forms::createFormFactory()
-        ->createBuilder('form')
-        ->add('face_image', 'file', [
-            'attr' => ['accept' => 'images/*']
-        ])
-        ->add('base_image', 'file', [
-            'attr' => ['accept' => 'images/*'],
-        ])
-        ->getForm();
+$app->get('/', function (Request $request) use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
 
-    $form->handleRequest();
+$app->post('/', function (Request $request) use ($app) {
+    try {
+        $faceImageJpeg = convert_image_to_jpeg($request->files->get('face_image'));
+        $faceImage = base64_encode(file_get_contents($faceImageJpeg));
 
-    if ($form->isValid()) {
-        try {
-            $files = $app['files'];
-            $faceImageJpeg = convert_image_to_jpeg($files['face_image']);
-            $faceImage = base64_encode(file_get_contents($faceImageJpeg));
+        $baseImageJpeg = convert_image_to_jpeg($request->files->get('base_image'));
+        $baseImage = base64_encode(file_get_contents($baseImageJpeg));
 
-            $baseImageJpeg = convert_image_to_jpeg($files['base_image']);
-            $baseImage = base64_encode(file_get_contents($baseImageJpeg));
+        $http = new GuzzleHttp\Client();
+        $result = $http->post('localhost:8081', [
+            'json' => [
+                'faceImage' => $faceImage,
+                'baseImage' => $baseImage,
+            ]
+        ]);
 
-            $http = new GuzzleHttp\Client();
-            $result = $http->get('faceswap-worker', [
-                'json' => [
-                    'faceImage' => $faceImage,
-                    'baseImage' => $baseImage,
-                ]
-            ]);
-
-            $twigVars['faceImage'] = $faceImage;
-            $twigVars['baseImage'] = $baseImage;
-            $twigVars['resultImage'] = $resultImage;
-        } catch (Exception $e) {
-            $twigVars['error'] = $e->getMessage();
-        }
+        $twigVars = [
+            'faceImage' => $faceImage,
+            'baseImage' => $baseImage,
+            'resultImage' => (string) $result->getBody(),
+        ];
+    } catch (Exception $e) {
+        $twigVars = ['error' => $e->getMessage()];
     }
 
-    $twigVars['form'] = $form->createView();
-    return $app['twig']->render('faceswap.html.twig', $twigVars);
+    return $app['twig']->render('index.html.twig', $twigVars);
 });
 
 return $app;
