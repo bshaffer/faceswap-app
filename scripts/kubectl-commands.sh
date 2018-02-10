565582c2d0ba5d6b522434824afb960a007e2a6e
Brent Shaffer <betterbrent@google.com>
2018-02-10 06:37:36 -0800

    updates scripts

--- scripts/kubectl-commands.sh
+++ scripts/kubectl-commands.sh
@@ -7,9 +7,30 @@ kubectl create secret docker-registry gcr-json-key \
 
 ### PHASE 1 ###
 kubectl run faceswap-app --image=gcr.io/sunshine-php-2018/faceswap-app --port=8080
-kubectl expose deployment faceswap-app --type LoadBalancer --port 80
+kubectl expose deployment faceswap-app --type LoadBalancer
 
 
 ### PHASE 2 ###
-kubectl create configmap pubsub --from-literal=PUBSUB_TOPIC=faceswap-images --from-literal=PUBSUB_SUBSCRIPTION=faceswap-worker
-kubectl create secret generic google-credentials --from-literal=cloud-next-php.json="$(cat ~/gcloud/creds/cloud-next-php.json)"
+kubectl create configmap pubsub \
+    --from-literal=PUBSUB_TOPIC=faceswap-images-local \
+    --from-literal=PUBSUB_SUBSCRIPTION=faceswap-worker-local
+
+kubectl create secret generic google-credentials \
+    --from-literal=sunshine-php-2018.json="$(cat ~/gcloud/creds/sunshine-php-2018.json)"
+
+kubectl create secret generic firebase-credentials \
+    --from-literal=apikey="$(cat ~/gcloud/creds/sunshine-php-2018-apikey.txt)"
+
+# for production
+kubectl create configmap pubsub \
+    --from-literal=PUBSUB_TOPIC=faceswap-images \
+    --from-literal=PUBSUB_SUBSCRIPTION=faceswap-worker
+
+kubectl create secret generic google-credentials \
+    --from-literal=sunshine-php-2018.json="$(cat ~/gcloud/creds/sunshine-php-2018.json)"
+
+kubectl create secret generic firebase-credentials \
+    --from-literal=apikey="$(cat ~/gcloud/creds/sunshine-php-2018-apikey.txt)"
+
+### PHASE EXTRA ###
+ kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.7.1/src/deploy/recommended/kubernetes-dashboard.yaml
